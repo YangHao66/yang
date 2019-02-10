a25fe162e6aa42d64492967d2ac23f1df0b98d5d
YangHao66 <519980326@qq.com>
2019-02-10 18:54:35 +0800

    goosd

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import useEle from '@/components/common/useEle.vue'
 import Login from '@/components/login/login.vue'
 import Home from '@/components/pages/home.vue'
 import User from '@/components/pages/user/user.vue'
+import Rights from '@/components/pages/rights/rights.vue'
+import Roles from '@/components/pages/roles/roles.vue'
+import Goods from '@/components/pages/goods/goods.vue'
+import GoodsAdd from '@/components/pages/goods/add.vue'
 
 Vue.use(Router)
 
@@ -35,8 +39,32 @@ export default new Router({
         {
           path: '/roles',
           component: Roles
+        },
+        {
+          path: '/goods',
+          component: Goods
+        },
+        {
+          path: '/goods/add',
+          component: GoodsAdd
         }
       ]
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== '/login') {
+    var token = window.localStorage.getItem('token')
+    if (!token) {
+      Message.error('您还没有登录，请先登录')
+      router.push('/login')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
